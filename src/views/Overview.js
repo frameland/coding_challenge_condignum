c7df992442792fe17db7b7fa845bded8bb96f1fd
Markus <markus.langthaler@gmx.net>
2022-07-17 15:35:45 +0200

    Added empty state to iteration liste

--- src/views/Overview.js
+++ src/views/Overview.js
@@ -21,28 +21,53 @@ export function Overview({ iterations, onNew, onIterationDelete }) {
 					New Iteration
 				</Button>
 			</div>
-			<ul>
-				{iterations
-					.sort(stringSort)
-					.map((i, index) => {
-						const wasCompleted = isIterationComplete(i.answers);
-						const path = wasCompleted ? 'read/' : 'iteration/';
-						console.log(path, i.answers);
-						return (
-							<li key={i.creationDateString}>
-								<IterationPreview
-									title={i.title}
-									creationDateString={i.creationDateString}
-									wasCompleted={wasCompleted}
-									path={path + index}
-									onDelete={() => onIterationDelete(index)}
-								/>
-							</li>
-						)
-					})
-				}
-			</ul>
+			<IterationList
+				iterations={iterations}
+				onDelete={onIterationDelete}
+			/>
 		</>
+	);
+}
+
+function IterationList({ iterations, onDelete }) {
+	if (iterations.length === 0) {
+		return <EmptyState />;
+	}
+
+	return (
+		<ul>
+			{iterations
+				.sort(stringSort)
+				.map((i, index) => {
+					const wasCompleted = isIterationComplete(i.answers);
+					const path = wasCompleted ? 'read/' : 'iteration/';
+					return (
+						<li key={i.creationDateString}>
+							<IterationPreview
+								title={i.title}
+								creationDateString={i.creationDateString}
+								wasCompleted={wasCompleted}
+								path={path + index}
+								onDelete={() => onDelete(index)}
+							/>
+						</li>
+					)
+				})
+			}
+		</ul>
+	);
+}
+
+function EmptyState(props) {
+	let classes = 'p-4 border bg-neutral-100 text-neutral-700 text-center h-1/2 border-2';
+
+	return (
+		<div className={classes}>
+			<h2 className="text-2xl font-bold mt-4">Welcome!</h2>
+			<p className="w-64 mx-auto mb-4">
+				Start a new iteration by clicking the <span className="font-bold">New Iteration</span> button.
+			</p>
+		</div>
 	);
 }
 
